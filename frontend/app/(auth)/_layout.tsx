429c4ccf31b5bd75e142d49e997391ffe33d6cc7
MahmoodMI1 <mahmood.idelbi1@icloud.com>
2026-04-30 20:14:57 -0400

    Frontend: added register tsx, and filled the auth layout

--- frontend/app/(auth)/_layout.tsx
+++ frontend/app/(auth)/_layout.tsx
@@ -1,10 +1,10 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
+export default function AuthLayout() {
   return (
     <Stack>
-      <Stack.Screen name="login" options={{ headerTitle: "Login" }} />
-      
+    <Stack.Screen name="login" options={{title: "Login"}} />
+    <Stack.Screen name="register" options={{title: "Register"}} />
     </Stack>
   );
 }
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,8 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{headerShown: false }}>
-      <Stack.Screen name="login" options={{ headerTitle: "Subscriptions" }} />
-      <Stack.Screen name="home" options={{ headerTitle: "About" }} />
+      <Stack.Screen name="login" />
+      <Stack.Screen name="home" />
     </Stack>
   );
 }
